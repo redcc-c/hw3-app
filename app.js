8ec5200ab6da3b9de305caed5ee4e927e65fb7b4
EC2 Default User <ec2-user@ip-172-31-29-127.ap-northeast-2.compute.internal>
2025-11-30 09:45:52 +0000

    Clean repo without runner

---
+++ app.js
@@ -0,0 +1,12 @@
+cat << 'EOF' > app.js
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello from GitHub Actions Deployment!\n");
+});
+
+server.listen(port, () => {
+  console.log("Server running on port " + port);
+});
